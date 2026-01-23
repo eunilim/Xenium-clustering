10a8d3949ef069d1308caa93016fa0aad8b619e0
Eunil Im <eim@lji.org>
2026-01-23 10:28:55 -0800

    change cat to echo to present nodelist

--- routine_template_emma_Slurm_Spatial_Clustering.sh
+++ routine_template_emma_Slurm_Spatial_Clustering.sh
@@ -27,7 +27,7 @@
 ######################################################################
 
 echo -----------------------------------------------------------------
-echo -n 'Job is running on node '; cat ${SLURM_NODELIST}
+echo -n 'Job is running on node '; echo ${SLURM_NODELIST}
 echo -----------------------------------------------------------------
 echo Slurm: sbacth is running on ${SLURM_SUBMIT_HOST}
 echo Slurm: originating queue is ${SLURM_JOB_PARTITION}
